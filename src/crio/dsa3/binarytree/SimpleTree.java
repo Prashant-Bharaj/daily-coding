ee0f09d29a24946bb7d9092612b212ea2cfac906
Prashant-Bharaj <prashant.bharaj@gmail.com>
2022-05-21 21:27:42 +0530

    tree problems

---
+++ src/crio/dsa3/binarytree/SimpleTree.java
@@ -0,0 +1,24 @@
+package crio.dsa3.binarytree;
+
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+    Node(int val, Node left, Node right){
+        this.val = val;
+        this.right = right;
+        this.left = left;
+    }
+    Node(int val){
+        this.val = val;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+public class SimpleTree {
+    public static void main(String[] args) {
+
+    }
+}
